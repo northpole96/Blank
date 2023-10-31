147c83249ad6eac7bee0967c77c297650631b5e8
Rajesh Khuntia <rajeshk001@icloud.com>
2023-10-31 16:59:42 +0530

    Initial Commit

---
+++ Blank/BlankApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlankApp.swift
+//  Blank
+//
+//  Created by Rajesh Khuntia on 31/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BlankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
